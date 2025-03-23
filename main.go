5a1efd6b14e3898e024e5bfa6aca9bce2c0a92ea
Harry Alvarado <harry2alvarado8@hotmail.com>
2025-03-23 14:51:54 -0500

    add front pages

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	http.HandleFunc("/shorten", handleShorten)
-	http.HandleFunc("/", handleRedirect)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "public/index.html")
+			return
+		}
+
+		http.ServeFile(w, r, "public"+r.URL.Path)
+	})
 
 	log.Println("Servidor escuchando en http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
