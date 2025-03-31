10e254904cce8d06aebe5dee92f33d99a6c4be75
Harry Alvarado <harry2alvarado8@hotmail.com>
2025-03-30 19:11:20 -0500

    feat: code

--- public/script.js
+++ public/script.js
@@ -1,11 +1,39 @@
+window.addEventListener('DOMContentLoaded', async () => {
+  const token = localStorage.getItem('token');
 
-// 2. Acortar URL con el token
+  if (!token) {
+    try {
+      const res = await fetch('/anon', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await res.json();
+
+      if (res.ok && data.token) {
+        localStorage.setItem('token', data.token);
+        console.log('[shortly] Sesión anónima iniciada');
+        document.getElementById('anonWarning').style.display = 'block';
+      } else {
+        console.error('Respuesta inesperada de /anon:', data);
+      }
+    } catch (error) {
+      console.error('No se pudo iniciar sesión anónima:', error);
+    }
+  } else {
+    // Si ya tiene sesión
+    document.getElementById('anonWarning').style.display = 'none';
+  }
+});
+
 async function shortenUrl() {
   const longUrl = document.getElementById('longUrl').value;
   const token = localStorage.getItem('token');
   const headers = { 'Content-Type': 'application/json' };
 
-  console.log("shortenUrl Token: ", token)
+  console.log('shortenUrl Token: ', token);
 
   if (token) {
     headers['Authorization'] = `Bearer-${token}`;
